08509f94d0328772c7aa078ec9db1b870a6d83c4
ShifatJahanShifa <bsse1301@iiit.du.ac.bd>
2025-03-07 16:40:52 +0600

    Day-5

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 
 // Middleware to parse JSON bodies
-app.use(express.json());
+// app.use(express.json());
 
 // Basic API endpoint
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
